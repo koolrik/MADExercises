3811acf97bd2cbe9c782624d44d43b24e677d673
Rikesh Karki <44747589+koolrik@users.noreply.github.com>
2019-02-17 21:29:15 +0545

    Assignment Completed.

--- Week1/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ Week1/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloconstraint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,12 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    Button button_Zero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        button_Zero = (Button) findViewById(R.id.button_Zero);
     }
 
     public void showToast(View view) {
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        button_Zero.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
@@ -32,5 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void showZero(View view) {
         mShowCount.setText(Integer.toString(0));
+        button_Zero.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
     }
 }
